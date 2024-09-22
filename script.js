888392d703b71b3624ec1c153a83572015adfa90
vaibha987 <v2goyal95@gmail.com>
2024-09-22 17:46:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function Person(name, age) {
 	this.age=age
 
 	this.greet=function(){
-		console.log(`Hello, my name is ${this.name}and I am ${this.age} years old.`)
+		console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`)
 	}
 }
 
