270c9ffaad58c1ab1bbbd5737695d2f1b0ecbef9
vaibha987 <v2goyal95@gmail.com>
2024-09-22 17:50:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -19,9 +19,9 @@ this.jobGreet=function(){
 Employee.prototype = Object.create(Person.prototype);
 Employee.prototype.constructor = Employee;
 const person = new Person('Alice', 25);
-person.greet(); // "Hello, my name is Alice, I am 30 years old."
+person.greet(); // "Hello, my name is Alice and I am 25 years old."
 const employee = new Employee('Bob', 30, 'Manager');
-employee.jobGreet();// "Hello, my name is Bob, I am 25 years old, and my job title is Software Engineer."
+employee.jobGreet();// "Hello, my name is Bob, I am 30 years old, and my job title is Manager"
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
