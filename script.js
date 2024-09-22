aadbc93e96bac0be08a1f299a503ecfa0b4d8cf8
vaibha987 <v2goyal95@gmail.com>
2024-09-22 17:33:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -15,7 +15,9 @@ jobGreet(){
 	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`)
 }
 	
-}	
+}
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
