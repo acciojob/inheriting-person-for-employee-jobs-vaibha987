a98bba250b1ef9623ea8f46b796d9d1e9b2eac0d
vaibha987 <v2goyal95@gmail.com>
2024-09-22 17:41:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -20,7 +20,8 @@ Employee.prototype = Object.create(Person.prototype);
 Employee.prototype.constructor = Employee;
 const person = new Person('Alice', 30);
 person.greet(); // "Hello, my name is Alice, I am 30 years old."
-const employee = new Employee('Bob', 25, 'Software Engineer');// "Hello, my name is Bob, I am 25 years old, and my job title is Software Engineer."
+const employee = new Employee('Bob', 25, 'Software Engineer');
+employee.jobGreet();// "Hello, my name is Bob, I am 25 years old, and my job title is Software Engineer."
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
