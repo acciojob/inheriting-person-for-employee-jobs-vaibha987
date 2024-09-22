e798cfd4ddeeb7bfd6ab3ea74fbff3f2d75f81d5
vaibha987 <v2goyal95@gmail.com>
2024-09-22 18:09:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
 // complete this js code
 function Person(name, age) {
-	this.name=name
-	this.age=age
+  this.name = name;
+  this.age = age;
 }
-
-Person.prototype.greet=function(){
-		console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`)
-	}
-
+Person.prototype.greet = function() {
+  console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+};
+ 
+ 
 function Employee(name, age, jobTitle) {
-Person.call(this,name,age)
-this.jobTitle=jobTitle
+  Person.call(this, name, age);
+  this.jobTitle = jobTitle;
 }
-Employee.prototype.jobGreet=function(){
-	console.log(`Hello, my name is ${this.name},I am ${this.age} years old, and my job title is ${this.jobTitle}`)
-}
+ 
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
+ 
+Employee.prototype.jobGreet = function() {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+};
+ 
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
