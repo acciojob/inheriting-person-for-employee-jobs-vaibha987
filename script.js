60577585cd6800751ce989249af42b3c70aaa12a
vaibha987 <v2goyal95@gmail.com>
2024-09-22 17:28:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	this.name=name
+	this.age=age
 
-function Employee(name, age, jobTitle) {}
+	greet(){
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
+	}
+}
 
+function Employee(name, age, jobTitle) {
+Person.call(name,age,jobTitle)
+this.jobTitle=jobTitle
+jobGreet(){
+	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`)
+}
+	
+}	
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
